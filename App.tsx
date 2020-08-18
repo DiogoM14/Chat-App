93696cb566d16188fe096fd99f1a2b532012a4a7
Xavi002 <diogomartins200214@gmail.com>
2020-08-18 17:26:05 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import ChatScreen from './src/Pages/ChatScreen';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ChatScreen />
+      <StatusBar style="dark" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/Pages/ChatScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+import { Text } from 'react-native';
+
+const ChatScreen: React.FC = () => {
+  return (
+    <Container>
+      <Text>Hello world</Text>
+    </Container>
+  );
+}
+
+export default ChatScreen;
